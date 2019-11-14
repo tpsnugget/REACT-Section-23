4f95cda639ea44df803169acd0f7d8292a54c6f5
Michael Giebner <mikegiebner@gmail.com>
2019-11-14 09:32:05 -0500

    Chapter 168 completed

---
+++ router-exercises-part-2/src/Routes.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+import { Route, Switch, Redirect } from "react-router-dom"
+import DogDetails from "./DogDetails"
+import DogList from "./DogList"
+
+class Routes extends Component {
+   render() {
+
+      const getDog = props => {
+         let name = props.match.params.name
+         let currentDog = this.props.dogs.find(
+           dog => dog.name.toLowerCase() === name.toLowerCase()
+         )
+         return(<DogDetails {...props} dog={currentDog} />)
+       }
+
+      return (
+         <Switch>
+            <Route exact path="/dogs" render={() => <DogList dogs={this.props.dogs} />} />
+            <Route exact path="/dogs/:name" render={getDog} />
+            <Redirect to="/dogs" />
+         </Switch>
+      )
+   }
+}
+
+export default Routes
